fbeaa0a1d138b176bec1a414a980370853020b2d
Fernando Lopez <77945753+2000fer@users.noreply.github.com>
2023-10-28 15:12:05 -0300

    Add files via upload

--- src/com/heroesyvillanos/Liga.java
+++ src/com/heroesyvillanos/Liga.java
@@ -1,43 +1,74 @@
 package com.heroesyvillanos;
 
 import java.util.List;
+import java.util.Map;
 
-public class Liga implements Competidor {
+public class Liga extends Competidor {
 	private String nombreLiga;
-	private List<Competidor> competidores; // puede contener personajes y ligas
-	private boolean tipoCompetidor; // true para heroes, false para villanos, cambiar por algo mejor
+	private List<Competidor> competidores;	
+	private Map<Caracteristica, Integer> cache_promedio_caracteristicas;
+	
+	Liga(String nombre, List<Competidor> competidores) throws Exception{
+		if(!esNombreValido(nombre)) {
+			throw new Exception("Nombre invalido");
+		}
+		
+		nombreLiga = nombre;
+		this.competidores = competidores;
+		this.updateCacheCaracteristicas();
+	}
+	
+	protected void agregarCompetidorALiga(Competidor c) throws Exception {
+		if(!tipoCompetidor.equals(c.tipoCompetidor)) {
+			throw new Exception("No se puede agregar un personaje/liga a una liga con distintos tipos de competidor"); 
+		}
+		
+		if(!c.puedeEntrarEnLiga()){
+			throw new Exception("Este personaje ya pertenece a una liga");
+		}
+		
+		competidores.add(c);
+		c.setEstaDentroDeLiga(true);
+		this.updateCacheCaracteristicas();
+	}
 	
 	@Override
-	public boolean esGanador(Competidor competidor, Caracteristica c) {
-		// Determina si la liga es ganadora contra otro competidor basandose en una caracteristica especifica. 
-		// Ojo que si da empate se usa la caracteristica que sigue.
-		return false;
+	protected int getPromedioCaracteristica(Caracteristica c) {
+		return cache_promedio_caracteristicas.get(c);
 	}
 	
-	public void agregarCompetidorALiga() {
-		// Agrega personaje o liga a otra liga
+	private void updateCacheCaracteristicas() {
+		int cantComp = this.getCantidadCompetidores();
+		for (Caracteristica c : Caracteristica.values()) {
+			int value = this.getSumaCaracteristica(c) / cantComp;
+			cache_promedio_caracteristicas.put(c, value);
+		}
 	}
 	
 	@Override
-	public int getPromedioCaracteristica(Caracteristica c) {
-		// Obtiene valor promedio de una caracteristica especifica en todos los competidores de la liga
-		return 0;
+	protected int getCantidadCompetidores() {
+		int sum = 0;
+		for(Competidor comp : competidores) {
+			sum += comp.getCantidadCompetidores();
+		}
+		return sum;
 	}
 	
-	// Getters y Setters
-	public String getNombreLiga() {
-		return nombreLiga;
+	protected int getSumaCaracteristica(Caracteristica c) {
+		int sum = 0;
+		for(Competidor comp : competidores) {
+			sum += comp.getSumaCaracteristica(c);
+		}
+		return sum;
 	}
-	
-	public void setNombreLiga(String nombreLiga) {
-		this.nombreLiga = nombreLiga;
-	}
-	
+		
 	public List<Competidor> getCompetidores() {
 		return competidores;
 	}
 	
-	public void setCompetidores(List<Competidor> competidores) {
-		this.competidores = competidores;
+	@Override
+	public String toString() {
+		return nombreLiga;
 	}
+	
 }
---
+++ src/com/heroesyvillanos/PersonajeTest.java
@@ -0,0 +1,14 @@
+package com.heroesyvillanos;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class PersonajeTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
